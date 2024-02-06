e1305d5e4d6584ebda66123bbc74ba153ca00aa5
Rohit <rohit.jha@razorpay.com>
2024-02-06 19:56:21 +0530

    Dice chnages and choose color content changed

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -10,9 +10,10 @@ import {
 } from "react-icons/fa";
 import { useState, useEffect } from "react";
 
-const Dice = ({ diceState, onDiceRoll,myColor,player3Color }) => {
+const Dice = ({ diceState, onDiceRoll, myColor, player3Color }) => {
   const [loading, setLoading] = useState(true);
-  const diceCurrentColor = diceState.whoseChance=='p1'?myColor:player3Color
+  const diceCurrentColor =
+    diceState.whoseChance == "p1" ? myColor : player3Color;
   useEffect(() => {
     // Show loading for 1 second
     setLoading(true);
@@ -25,39 +26,67 @@ const Dice = ({ diceState, onDiceRoll,myColor,player3Color }) => {
 
   return (
     <>
-    <div className="dice-button-container">
-      <div className='dice-container'>
-      {loading ? 
-
-          <FaSquare size={50} className="spinning-dice" style={{borderRadius:'70%', animationDuration: '0.5s' }} />
-       : (
-        <>
-          {diceState.value === "2" ? <FaDiceTwo size={50}  style={{backgroundColor:diceCurrentColor,color:'black'}}/> : ""}
-          {diceState.value === "3" ? <FaDiceThree size={50} style={{backgroundColor:diceCurrentColor,color:'black'}} /> : ""}
-          {diceState.value === "4" ? <FaDiceFour size={50} style={{backgroundColor:diceCurrentColor,color:'black'}} /> : ""}
-          {diceState.value === "5" ? <FaDiceFive size={50} style={{backgroundColor:diceCurrentColor,color:'black'}} /> : ""}
-          {diceState.value === "6" ? <FaDiceSix size={50} style={{backgroundColor:diceCurrentColor,color:'black'}} /> : ""}
-          {diceState.value === "1" || diceState.value === "" ? (
-            <FaDiceOne size={50} style={{backgroundColor:diceCurrentColor,color:'black'}} />
+      <div className="dice-button-container">
+        <div className="dice-container">
+          {loading ? (
+            <FaSquare
+              size={50}
+              className="spinning-dice"
+              style={{ borderRadius: "70%", animationDuration: "0.5s" }}
+            />
           ) : (
-            ""
+            <>
+              {diceState.value === "2" ? (
+                <FaDiceTwo size={50} style={{ backgroundColor:diceCurrentColor, color: 'black' }} />
+              ) : (
+                ""
+              )}
+              {diceState.value === "3" ? (
+                <FaDiceThree size={50} style={{ backgroundColor:diceCurrentColor, color: 'black' }} />
+              ) : (
+                ""
+              )}
+              {diceState.value === "4" ? (
+                <FaDiceFour size={50} style={{ backgroundColor:diceCurrentColor, color: 'black' }} />
+              ) : (
+                ""
+              )}
+              {diceState.value === "5" ? (
+                <FaDiceFive size={50} style={{ backgroundColor:diceCurrentColor, color: 'black' }} />
+              ) : (
+                ""
+              )}
+              {diceState.value === "6" ? (
+                <FaDiceSix size={50} style={{ backgroundColor:diceCurrentColor, color: 'black' }} />
+              ) : (
+                ""
+              )}
+              {diceState.value === "1" || diceState.value === "" ? (
+                <FaDiceOne size={50} style={{ backgroundColor:diceCurrentColor, color: 'black' }} />
+              ) : (
+                ""
+              )}
+            </>
           )}
-          
-        </>
-      )}
-      </div>  
-      {diceState.canbeRolled ? (
-        <button type="button" className="button-class" style={{backgroundColor:diceCurrentColor,color:'black'}} onClick={onDiceRoll}>
-          Roll Dice
-        </button>
-      ) : ''
-      }
-    </div>
-       {/* <div className="arrow-container">
+        </div>
+        {diceState.canbeRolled ? (
+          <button
+            type="button"
+            className="button-class"
+            style={{ backgroundColor: diceCurrentColor, color: "black" }}
+            onClick={onDiceRoll}
+          >
+            Roll Dice
+          </button>
+        ) : (
+          ""
+        )}
+      </div>
+      {/* <div className="arrow-container">
     <p>{diceState.whoseChance==='p1'?'Player 1':"Player 3"}</p>
        <FaArrowRight className="arrow-icon" style={{color:diceState.whoseChance==='p1'?myColor:player3Color}}/>
      </div> */}
-     </>
+    </>
   );
 };
 
